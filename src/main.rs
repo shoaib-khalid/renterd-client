602d570877eab55b78f254e19c99c87642f2fe7e
shoaib-khalid <shoaibkhaild@gmail.com>
2024-02-16 23:01:46 +0500

    added get state and upload_file methods

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,27 @@
+use reqwest::header::{HeaderMap, HeaderValue, AUTHORIZATION, CONTENT_TYPE};
+use reqwest::Client;
 use std::fs::File;
 use std::io::Read;
-use reqwest::Client;
-use reqwest::header::{HeaderMap, HeaderValue, AUTHORIZATION, CONTENT_TYPE};
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
-    upload_file("http://localhost:9980", "/api/worker", "Texts", "D:\\Fawad_file_renterd1.txt").await?;
+    let worker_uri: &str = "http://localhost:9880";
+    let worker_api_prefix: &str = "/api";
+
+    get_state(worker_uri, worker_api_prefix).await?;
+    upload_file(worker_uri, "/api/worker", "Texts", "D:\\Fawad_file_renterd1.txt").await?;
+    
     Ok(())
 }
 
 //Todo build the basic api status method and see if it is able to connect or not
 
-pub async fn upload_file(worker_uri: &str, worker_api_prefix: &str, key: &str, file_path: &str) -> Result<(), Box<dyn std::error::Error>> {
+pub async fn upload_file(
+    worker_uri: &str,
+    worker_api_prefix: &str,
+    key: &str,
+    file_path: &str,
+) -> Result<(), Box<dyn std::error::Error>> {
     // Open the file
     let mut file = File::open(file_path)?;
 
@@ -23,19 +33,21 @@ pub async fn upload_file(worker_uri: &str, worker_api_prefix: &str, key: &str, f
     let client = Client::new();
 
     // Create the URL
-    let url = format!("{}{}/objects/PBS-Cloud-Backup/{}", worker_uri, worker_api_prefix, key);
+    let url = format!(
+        "{}{}/objects/PBS-Cloud-Backup/{}",
+        worker_uri, worker_api_prefix, key
+    );
 
     // Create the headers
     let mut headers = HeaderMap::new();
     headers.insert(CONTENT_TYPE, HeaderValue::from_static("text/plain"));
-    headers.insert(AUTHORIZATION, HeaderValue::from_static("Basic OnByb3htb3hpc2dyODJkYXkh"));
+    headers.insert(
+        AUTHORIZATION,
+        HeaderValue::from_static("Basic OnByb3htb3hpc2dyODJkYXkh"),
+    );
 
     // Send the request
-    let res = client.put(&url)
-        .headers(headers)
-        .body(buf)
-        .send()
-        .await?;
+    let res = client.put(&url).headers(headers).body(buf).send().await?;
 
     // Check the status
     if res.status().is_success() {
@@ -47,6 +59,7 @@ pub async fn upload_file(worker_uri: &str, worker_api_prefix: &str, key: &str, f
     Ok(())
 }
 
+
 pub async fn get_state(worker_uri: &str, worker_api_prefix: &str) -> Result<(), Box<dyn std::error::Error>> {
     // Create a client
     let client = Client::new();
@@ -54,22 +67,39 @@ pub async fn get_state(worker_uri: &str, worker_api_prefix: &str) -> Result<(), 
     // Create the URL
     let url = format!("{}{}/bus/state", worker_uri, worker_api_prefix);
 
+    // Print the URL
+    println!("URL: {}", url);
+
     // Create the headers
     let mut headers = HeaderMap::new();
     headers.insert(AUTHORIZATION, HeaderValue::from_static("Basic OnByb3htb3hpc2dyODJkYXkh"));
 
+    // Print the headers
+    println!("Headers: {:?}", headers);
+
     // Send the request
     let res = client.get(&url)
-        .headers(headers)
+        .headers(headers.clone())
         .send()
         .await?;
 
+    // Print the response status
+    println!("Response status: {}", res.status());
+
+    // Print the response headers
+    println!("Response headers: {:?}", res.headers());
+
     // Check the status
     if res.status().is_success() {
-        println!("Request was successful");
+        // Get the response body as text
+        let body = res.text().await?;
+
+        // Print the response body
+        println!("Response body: {}", body);
     } else {
         println!("Failed to send request: {}", res.status());
     }
 
     Ok(())
 }
+
